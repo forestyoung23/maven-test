b0d66702b5b62b912bcbf7f94b023d2faecb9e6a
dongyang <dongyang@gdsunfly.com>
2021-10-22 18:17:05 +0800

    Initial commit

---
+++ maven-provide/src/main/java/com/forest/CommonUtils.java
@@ -0,0 +1,11 @@
+package com.forest;
+
+/**
+ * @author Forest Dong
+ * @date 2021年10月22日 17:18
+ */
+public class CommonUtils {
+    public static String getStr() {
+        return "sdf";
+    }
+}
---
+++ maven-service/src/main/java/com/forest/Demo.java
@@ -0,0 +1,11 @@
+package com.forest;
+
+/**
+ * @author Forest Dong
+ * @date 2021年10月22日 17:34
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.err.println(CommonUtils.getStr());
+    }
+}
